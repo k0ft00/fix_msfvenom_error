80a75c54e49d89e4fb25f45948cf929bc9a79dc6
k0ft00 <kenacslklqenflacks@outlook.com>
2024-07-26 01:52:34 +0300

    Add files via upload

---
+++ bash_file/apktool.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool > apktool
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.3.jar
+mv apktool_2.9.3.jar apktool.jar
+chmod +x apktool apktool.jar
+sudo apt purge apktool -y
+sudo mv apktool apktool.jar /usr/local/bin
---
+++ fix_error.py
@@ -0,0 +1,46 @@
+import os
+
+print(
+    """
+,,
+`""*$b..
+     ""*$o.
+         "$$o.
+           "*$$o.
+              "$$$o.
+                "$$$$bo...       ..o:
+                  "$$$$$$$$booocS$$$    ..    ,.
+               ".    "*$$$$SP     V$o..o$$. .$$$b
+                "$$o. .$$$$$o. ...A$$$$$$$$$$$$$$b
+          ""bo.   "*$$$$$$$$$$$$$$$$$$$$P*$$$$$$$$:
+             "$$.    V$$$$$$$$$P"**""*"'   VP  * "l
+               "$$$o.4$$$$$$$$X
+                "*$$$$$$$$$$$$$AoA$o..oooooo..           .b
+                       .X$$$$$$$$$$$P""     ""*oo,,     ,$P
+                      $$P""V$$$$$$$:    .        ""*****"
+                    .*"    A$$$$$$$$o.4;      .
+                         .oP""   "$$$$$$b.  .$;
+                                  A$$$$$$$$$$P
+                                  "  "$$$$$P"
+                                      $$P*"
+                                     .$"
+                                     "
+1. apktool
+2. zipalign
+3. apksigner
+"""
+)
+print("tele: @Cif_3")
+w = input("Enter a number your problem : ")
+print(os.popen("sudo apt update").read())
+if w == "1":
+    os.system("bash bash_file/apktool.sh")
+    print("apktool is solved")
+elif w == "2":
+    os.system("bash bash_file/zipalign.sh")
+    print("zipalign is solved")
+elif w == "3":
+    os.system("bash bash_file/apksigner.sh")
+    print("apksigner is solved")
+else:
+    print("not found")
