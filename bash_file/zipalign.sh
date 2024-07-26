bc1fe5d15a245bf40e98af8a785845e1e17818a8
Cif3 <kenacslklqenflacks@outlook.com>
2024-07-26 15:08:41 +0300

    Update zipalign.sh

--- bash_file/zipalign.sh
+++ bash_file/zipalign.sh
@@ -2,4 +2,5 @@
 sudo apt update
 sudo apt --purge remove zipalign -y
 wget http://ftp.de.debian.org/debian/pool/main/a/android-platform-build/zipalign_8.1.0+r23-2_amd64.deb
+chmod +x zipalign_8.1.0+r23-2_amd64.deb
 sudo apt install ./zipalign_*_amd64.deb -y
